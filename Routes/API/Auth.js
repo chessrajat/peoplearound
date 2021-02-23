a74325991b58fca1bbca00ae2f32d8c280659b16
Rajat Tyagi <raat66rat@gmail.com>
2021-02-23 18:10:41 +0530

    login and profile model added

--- Routes/API/Auth.js
+++ Routes/API/Auth.js
@@ -1,11 +1,17 @@
 import express from "express";
+import { check, validationResult } from "express-validator";
 import authMiddleware from "../../Middleware/Auth";
 import userModel from "../../Models/UserModel";
+import bcrypt from "bcryptjs";
+import config from "../../config/default.json"
+import jwt from "jsonwebtoken";
+
+
 const router = express.Router();
 
 // @route     GET  api/auth
-// @desc      Just a test route
-// @access    Public
+// @desc      Get the user info
+// @access    private
 router.get("/", authMiddleware, async (req, res) => {
   // using try catch because calling db
   try {
@@ -16,5 +22,65 @@ router.get("/", authMiddleware, async (req, res) => {
     res.status(500).json({ msg: "Internal server error" });
   }
 });
+
+// @route     POST  api/auth
+// @desc      Login a user and get token
+// @access    Public
+router.post(
+  "/",
+  [
+    // adding a express validator in as middleware.
+    // to validate input
+    check("email", "Please Include a valid Email Address").isEmail(),
+    check("password", "Password is required").exists(),
+  ],
+  async (req, res) => {
+    // handle the request check
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { email, password } = req.body;
+    try {
+      // check if the user exists
+      const user = await userModel.findOne({ email: email });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invalid Credentials" }] });
+      }
+
+      // match user email and password.
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invalid Credentials" }] });
+      }
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      // return the jwt
+      jwt.sign(
+        payload,
+        config.jwttoken,
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) {
+            throw err;
+          }
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
 
 export { router as authRouter };
--- Routes/API/Profile.js
+++ Routes/API/Profile.js
@@ -1,11 +1,27 @@
 import express from "express";
+import authMiddleware from "../../Middleware/Auth";
+import profileModel from "../../Models/ProfileModel";
 const router = express.Router();
 
-// @route     GET  api/profile
-// @desc      Just a test route
-// @access    Public
-router.get("/", (req, res) => {
-  res.send("Profile route");
+// @route     GET  api/profile/me
+// @desc      Get current user profile
+// @access    Private
+router.get("/me", authMiddleware, async (req, res) => {
+  try {
+    // adding other collection data in same object query
+    const profile = await profileModel
+      .findOne({ user: req.user.id })
+      .populate("user", ["name", "avatar"]);
+
+    if (!profile) {
+      return res.status(400).json({ msg: "There is no profile for this user" });
+    }
+
+    return res.json(profile);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 export { router as profileRouter };
