cef8cafb152421fe6c85d406dbd23610bb29c5c0
Rajat Tyagi <raat66rat@gmail.com>
2021-02-26 22:56:10 +0530

    added: posts model and api add new post

--- Routes/API/Posts.js
+++ Routes/API/Posts.js
@@ -1,11 +1,37 @@
 import express from "express";
+import { check, validationResult } from "express-validator";
+import authMiddleware from "../../Middleware/Auth";
+import postModal from "../../Models/PostModel";
+import userModel from "../../Models/UserModel";
 const router = express.Router();
 
-// @route     GET  api/posts
-// @desc      Just a test route
-// @access    Public
-router.get("/", (req, res) => {
-  res.send("Posts route");
-});
+// @route     POST  api/posts
+// @desc      Create a post
+// @access    Private
+router.post(
+  "/",
+  [authMiddleware, [check("text", "Text is required").not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await userModel.findById(req.user.id).select("-password");
+      const newPost = new postModal({
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id,
+      });
+      const post = await newPost.save();
+      res.json(post);
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send("Internal server Error");
+    }
+  }
+);
 
 export { router as postsRouter };
