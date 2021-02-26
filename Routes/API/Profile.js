e9eabb41a9c5f3f5ff6d20eeb369dafd6783026b
Rajat Tyagi <raat66rat@gmail.com>
2021-02-26 18:30:00 +0530

    added: add and delete education in profile

--- Routes/API/Profile.js
+++ Routes/API/Profile.js
@@ -223,7 +223,7 @@ router.put(
 );
 
 // @route     DELETE api/profile/experience/:exp_id
-// @desc      Add Profile experience
+// @desc      delete Profile experience
 // @access    Private
 router.delete("/experience/:exp_id", [authMiddleware], async (req, res) => {
   const isValidId = mongoose.Types.ObjectId.isValid(req.params.exp_id);
@@ -240,6 +240,83 @@ router.delete("/experience/:exp_id", [authMiddleware], async (req, res) => {
       .indexOf(exp_id);
 
     profile.experience.splice(removeIndex, 1);
+    await profile.save();
+    res.json(profile);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send("Internal server Error");
+  }
+});
+
+// @route     PUT api/profile/education
+// @desc      Add Profile Education
+// @access    Private
+router.put(
+  "/education",
+  [
+    authMiddleware,
+    [
+      check("school", "School is required").not().isEmpty(),
+      check("degree", "Degree is required").not().isEmpty(),
+      check("fieldofstudy", "Field of study is required").not().isEmpty(),
+      check("from", "From Date is required").not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const {
+      school,
+      degree,
+      fieldofstudy,
+      from,
+      to,
+      current,
+      description,
+    } = req.body;
+
+    const newEdu = {
+      school,
+      degree,
+      fieldofstudy,
+      from,
+      to,
+      current,
+      description,
+    };
+
+    try {
+      const profile = await profileModel.findOne({ user: req.user.id });
+      profile.education.unshift(newExp);
+      await profile.save();
+      res.json(profile);
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send("Internal server Error");
+    }
+  }
+);
+
+// @route     DELETE api/profile/education/:edu_id
+// @desc      delete Profile education
+// @access    Private
+router.delete("/education/:edu_id", [authMiddleware], async (req, res) => {
+  const isValidId = mongoose.Types.ObjectId.isValid(req.params.exp_id);
+  if (!isValidId) {
+    return res.status(400).json({ msg: "There is no Education with this id" });
+  }
+  const edu_id = req.params.edu_id;
+
+  try {
+    const profile = await profileModel.findOne({ user: req.user.id });
+    // Get the remove index
+    const removeIndex = profile.education
+      .map((item) => item.id)
+      .indexOf(exp_id);
+
+    profile.education.splice(removeIndex, 1);
     await profile.save();
     res.json(profile);
   } catch (err) {
