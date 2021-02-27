4721f8cfa7ebb46b7c5f4e4949f89cea69842f90
Rajat Tyagi <raat66rat@gmail.com>
2021-02-27 08:33:42 +0530

    added: api endpoint for get all posts, get post by id, delete post

--- Routes/API/Posts.js
+++ Routes/API/Posts.js
@@ -34,4 +34,64 @@ router.post(
   }
 );
 
+// @route     GET  api/posts
+// @desc      Get All Posts
+// @access    Private
+
+router.get("/", authMiddleware, async (req, res) => {
+  try {
+    const posts = await postModal.find().sort({ date: -1 });
+    res.json(posts);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send("Internal server Error");
+  }
+});
+
+// @route     GET  api/posts/:post_id
+// @desc      Get post by id
+// @access    Private
+router.get("/:post_id", authMiddleware, async (req, res) => {
+  try {
+    const post = await postModal.findById(req.params.post_id);
+    if (!post) {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+    res.json(post);
+  } catch (err) {
+    console.log(err.message);
+    if (err.kind == "ObjectId") {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+    res.status(500).send("Internal server Error");
+  }
+});
+
+// @route     DELETE  api/posts/:post_id
+// @desc      Delete a post
+// @access    Private
+
+router.delete("/:post_id", authMiddleware, async (req, res) => {
+  try {
+    const post = await postModal.findById(req.params.post_id);
+
+    if (!post) {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+    
+    if (post.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "User not authorised" });
+    }
+    await post.remove();
+
+    res.json({ msg: "Post removed" });
+  } catch (err) {
+    console.log(err.message);
+    if (err.kind == "ObjectId") {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+    res.status(500).send("Internal server Error");
+  }
+});
+
 export { router as postsRouter };
