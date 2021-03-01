a378975df0686f841075e933ae66a24430d2f957
Rajat Tyagi <raat66rat@gmail.com>
2021-03-01 14:11:07 +0530

    added: client - redux store added

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,19 @@
+import { Route } from "react-router-dom";
 import "./App.css";
+import Login from "./Components/Auth/Login";
+import Register from "./Components/Auth/Register";
 
 function App() {
-  return <div> App </div>;
+  return (
+    <>
+      <Route path="/register" exact>
+        <Register />
+      </Route>
+      <Route path="/login" exact>
+        <Login />
+      </Route>
+    </>
+  );
 }
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,19 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import { configureStore } from "./Utils/ConfigureStore";
+import { Provider } from "react-redux";
+
+const store = configureStore();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
